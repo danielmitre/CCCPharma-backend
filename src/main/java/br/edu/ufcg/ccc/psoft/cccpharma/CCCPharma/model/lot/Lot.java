eef502692a624df0d6218c7f564ac0a07e48c35e
Daniel Mitre <daniel.mitre@ccc.ufcg.edu.br>
2018-12-15 21:31:58 -0200

    changing lot to @embeddable

--- src/main/java/br/edu/ufcg/ccc/psoft/cccpharma/CCCPharma/model/lot/Lot.java
+++ src/main/java/br/edu/ufcg/ccc/psoft/cccpharma/CCCPharma/model/lot/Lot.java
@@ -76,29 +76,20 @@ public class Lot {
         return this.shelfLife.after(currentTime);
     }
 
+
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + amount;
+		result = prime * result + ((shelfLife == null) ? 0 : shelfLife.hashCode());
 		return result;
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Lot other = (Lot) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
+		return this == obj;
 	}
 
 	@Override
